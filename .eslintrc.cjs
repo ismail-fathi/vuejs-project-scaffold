f856eb2ff508e5e38ec665470fcc1734821a020c
ismail.fathi <ismail.fathi@tui.com>
2024-06-10 14:59:44 +0100

    feat(eslint): add ignorePatterns & rules

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -11,5 +11,16 @@ module.exports = {
   ],
   parserOptions: {
     ecmaVersion: 'latest'
-  }
+  },
+  ignorePatterns: [
+    '**/node_modules/**',
+    '**/dist/**',
+    '**/public/**',
+    '**/functions/lib/**',
+  ],
+  rules: {
+    // Warn on console.log, but allow other console methods
+    'no-console': [process.env.NODE_ENV === 'production' ? 'error' : 'warn', { allow: ['warn', 'error', 'info'] }],
+    "vue/attributes-order": 'error'
+  },
 }
